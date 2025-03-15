6b2c01dae5c60bf0adeb75d5cf3e806e3c1f8940
artemazamatov <azamatovartem@gmail.com>
2025-03-15 15:04:02 +0100

    Improved efficiency

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use clap::Parser;
 use crate::pihole::update_pihole_database;
 
 #[derive(Parser)]
-#[command(name = "LAN_check")]
-#[command(about = "Checks LAN connectivity", version = "1.0", author = "Artem Azamatov <azamatovartem@gmail.com>")]
+#[command(name = "rustypi")]
+#[command(about = "Checks network connectivity, mounts and updates pi-hole gravity list", version = "1.1", author = "Artem Azamatov <azamatovartem@gmail.com>")]
 struct Args {
     ip: Option<String>,
 }
@@ -17,6 +17,7 @@ fn main() {
     if  !network::check_and_fix_network(args) {
         print!("Network is NOK")
     }
+
     if !mount::check_and_fix_mount() {
         print!("Mount is NOK")
     }
